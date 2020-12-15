3f4e7e7b19b4bf1dd78691f58ad41868e13267ba
Adam <alane019@gmail.com>
2020-12-15 18:25:37 -0500

    intial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// array of questions for user
+const questions = [
+//Any and all questions that have to be answered when preparing a readme file. 
+
+// repository name (with dashes, not spaces)
+	// e.g., readme-generator-cli
+	 //This will be used for: Title heading, Deployment URLs, and Proper-case/space version in Table of Contents.
+
+// provide short description including the purpose of the repostory; 
+
+// [potentially, ask for path the screenshot]
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+ //add js here to write specified data to specified readme file. 
+}
+
+// function to initialize program
+function init() {
+
+ // js to ask questions, and prepares response data to be passed to the writeFile function above; 
+
+
+}
+
+// function call to initialize program
+init();

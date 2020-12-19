4b4a8ead182e357d8e95b3c1074b2d8632b45d42
Adam <alane019@gmail.com>
2020-12-19 03:51:34 -0500

    update init and writeToFile functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const util = require('util'); 
+const generateMarkdown = require('./utils/generateMarkdown');
 // array of questions for user
 const questions = () => { return inquirer.prompt([
 //Any and all questions that have to be answered when preparing a readme file. 
@@ -8,53 +9,55 @@ const questions = () => { return inquirer.prompt([
 	 type: 'input',
 	 name: 'readmeTitleHeading',
 	 message: "Name of repository as it should appear in H1 heading at the top of readme: ",
-	},
-	{
+	},{
 	type: 'input',
 	name: 'repoName',
 	message: "Name of repository as it should in the Github URL: ",
-	},
-	{
+	},{
 	type: 'input',
 	name: 'overviewDescription',
 	message: "Short description of ",
-	},
-	{
+	},{
 	type: 'input',
 	name: 'installation',
 	message: "Installation instructions: ",
-	}, //Installation, Usage, License, Contributing, Tests, and Questions
-	{
+	},{
 	type: 'input',
 	name: 'usage',
 	message: "Usage instructions: ",
-	},
-	{
+	},{
 	type: 'input',
 	name: 'contribution',
 	message: "How to contribute: ",
-	},
-	{
+	},{
 	type: 'input',
 	name: 'githubUsername',
 	message: "Github username: ",
 	},
-])};
+])
+
+
+}; // end questions; 
 
 // function to write README file
 function writeToFile(fileName, data) {
  //add js here to write specified data to specified readme file. 
-
-
+	fs.writeToFile(fileName, data, function(err){
+		if(err){
+			console.log(err); 
+		} else {
+			console.log("Success:  File was created!")
+		}
+	})
 }
 
 // function to initialize program
 function init() {
-
- // js to ask questions, and prepares response data to be passed to the writeFile function above; 
-
-
-}
+	inquirer.prompt(questions)
+	.then(answers => {
+		writeToFile("readme.md", generateMarkdown(answers))
+	});
+}	
 
 // function call to initialize program
 init();
